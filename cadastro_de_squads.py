b70ba4f8a403ca1884efd51269cd9fd1f4cfd61d
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2022-06-13 11:41:41 -0300

    Conserto de bugs e erros

--- cadastro_de_squads.py
+++ cadastro_de_squads.py
@@ -1,4 +1,3 @@
-
 #######################################
 # CRIANDO CLASSES
 #######################################
@@ -7,12 +6,14 @@ class Pessoa:
     def __init__(self, nome, fone):
         self.nome = nome
         self.fone = fone
+
     def exibir(self):
         print(f'-> {self.nome} - {self.fone}')
 
+
 class Squad:
-    def __init__(self, nome, techlead = None, devs=None):
-        self.nome =  nome
+    def __init__(self, nome, techlead=None, devs=None):
+        self.nome = nome
         self.devs = []
         self.techlead = techlead
 
@@ -22,6 +23,7 @@ class Squad:
     def incluir_dev(self, dev):
         self.devs.append(dev)
 
+
 class Colaborador(Pessoa):
     def __init__(self, nome, fone, squad=None):
         super().__init__(nome, fone)
@@ -30,10 +32,11 @@ class Colaborador(Pessoa):
     def incluir_squad(self, squad):
         self.squad = squad
 
+
 class Dev(Colaborador):
     def __init__(self, nome, fone, cargo, squad=None):
-        super().__init__(self, nome, fone, squad)
-        self.cargo =  cargo
+        super().__init__( nome, fone, squad)
+        self.cargo = cargo
 
     def exibir(self):
         super().exibir()
@@ -46,15 +49,16 @@ print('Bem vindo ao sistema de cadastro de squads! \n')
 #######################################
 # LOOP DE CADASTRO
 #######################################
+squads = []
+
 while True:
     #######################################
     # ADICIONANDO SQUAD
     #######################################
-    squad = []
 
-    nome_squad = input('\n Nome do squad: ')
-    nome_techlead =  input('\n Nome do techlead da squad: ')
-    fone_techlead = input('\n Telefone do techlead: ')
+    nome_squad = input('\nNome do squad: ')
+    nome_techlead = input('Nome do techlead da squad: ')
+    fone_techlead = input('Telefone do techlead: ')
 
     squad = Squad(nome_squad)
     techlead = Colaborador(nome_techlead, fone_techlead)
@@ -62,32 +66,40 @@ while True:
     #######################################
     # ADICIONANDO TECHLEAD
     #######################################
-    techlead = Colaborador(nome_techlead,fone_techlead)
+    techlead = Colaborador(nome_techlead, fone_techlead)
     squad.incluir_techlead(techlead)
     techlead.incluir_squad(squad)
 
-    squad.append(squad)
+    squads.append(squad)
 
     #######################################
     # ADICIONANDO DESENVOLVEDOR
     #######################################
     while True:
-        nome_dev = input('\n Nome do desenvolvedor: ')
-        fone_dev = input('\n Telefone do desenvolvedor: ')
-        cargo_dev = input('\n Cargo do desenvolvedor: ')
+        nome_dev = input('\nNome do desenvolvedor: ')
+        fone_dev = input('Telefone do desenvolvedor: ')
+        cargo_dev = input('Cargo do desenvolvedor: ')
         dev = Dev(nome_dev, fone_dev, cargo_dev)
         dev.incluir_squad(squad)
         squad.incluir_dev(dev)
-        option
-        option = input('\n Deseja adicionar mais um desenvolvedor [S\N]: ')
+
+        #######################################
+        # OPÇÃO DE RETORNO
+        #######################################
+        option = input(r'Deseja adicionar mais um desenvolvedor [S\N]: ')
         if option in 'Nn':
             break
 
-    option = input("\n Deseja adicionar mais um squad [S\N]: ")
+    option = input(r"Deseja adicionar mais um squad [S\N]: ")
     if option in 'Nn':
         break
 
-for squad in squad:
+#######################################
+# SQUADS CRIADOS E FINALIZADOS
+#######################################
+print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+print('\nSquads criados: ')
+for squad in squads:
     print(f'\n-----------------------------{nome_squad}--------------------------------------')
     print(f'TechLead: {squad.techlead.nome}')
     print('------------Devs do Squad--------------')
@@ -96,6 +108,3 @@ for squad in squad:
     print(f'-----------------------------{nome_squad}--------------------------------------')
 
 print('\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Sky.One Solution -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
-
-
-
